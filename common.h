afa450d33f43cfe23b336d30a6843e4692dba909
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-08-20 07:30:09 -0500

    Add files

---
+++ common.h
@@ -0,0 +1,30 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+/*=======STANDARD TEMPLATE LIBRARY=======*/
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <memory>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+/*=======GLU/GLEW=======*/
+
+#include "./Dependencies/include/GL/glew.h"
+#include "./Dependencies/include/GL/glu.h"
+#include "./Dependencies/include/GL/gl.h"
+
+/*=======SDL2=======*/
+
+#include "./Dependencies/include/SDL2/SDL.h"
+#include "./Dependencies/include/SDL2/SDL_opengl.h"
+#include "./Dependencies/include/SDL2/SDL_ttf.h"
+#include "./Dependencies/include/SDL2/SDL_image.h"
+
+/*=======CLASS DEFINITIONS=======*/
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include "common.h"
+
+bool isRunning = true;
+
+int WinMain()
+{
+    //Error Checking/Initialisation
+    if (SDL_Init(SDL_INIT_NOPARACHUTE & SDL_INIT_EVERYTHING) != 0) {
+        SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
+        return -1;
+    }
+    else
+    {
+        //Specify OpenGL Version (4.2)
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_Log("SDL Initialised");
+    }
+
+    //Create Window Instance
+    SDL_Window* window = SDL_CreateWindow(
+        "Game Engine",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640,
+        480,
+        SDL_WINDOW_OPENGL
+    );
+
+    //Check that the window was succesfully created
+    if(window == NULL)
+    {
+        //Print error, if null
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+    else
+        SDL_Log("Window Successful Generated");
+
+
+    //Map OpenGL Context to Window
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    //Render
+
+    //Swap Render Buffers
+    SDL_GL_SwapWindow(window);
+
+
+    //Free up resources
+    SDL_GL_DeleteContext(glContext);
+    SDL_Quit();
+
+    return 0;
+}
