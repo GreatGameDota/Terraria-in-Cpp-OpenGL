d46522f302a49da31626f38a5a5387daff35ed47
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2019-08-20 20:52:26 -0500

    Fix lib files

--- main.cpp
+++ main.cpp
@@ -179,6 +179,6 @@ void loadSurface(std::string path)
     }
 
     SDL_Log("Image successfully loaded");
-    auto name = path.substr(7, path.length() - 11);
+    string name = path.substr(7, path.length() - 11);
     images.insert(pair<string, SDL_Surface *>(name, optimizedSurface));
 }
